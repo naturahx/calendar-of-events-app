e5036d80cecb77bad062f3c0d5deb8754dcc63cb
Artuur Alexandrov <naturahxq@gmail.com>
2024-03-12 20:24:25 +0300

    login styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
+import "./App.css";
+import { Box, Button, FormControl, TextField } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: any) => {
+    event.preventDefault();
+  };
+
+  const [loading, setLoading] = useState(false);
+  function handleClick() {
+    setLoading(true);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className="header">
+        <div className="user">FRFR</div>
+        <button className="login">Login</button>
+      </header>
+      <div className="container">
+        <div className="form">
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              m: 13,
+            }}
+          >
+            <TextField
+              sx={{ m: 1, width: "50ch" }}
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+            />
+            <FormControl sx={{ m: 1, width: "50ch" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <LoadingButton
+              onClick={handleClick}
+              loading={loading}
+              loadingIndicator="Loading…"
+              variant="outlined"
+              sx={{ width: "200px", m: 2 }}
+            >
+              <span>Sign in</span>
+            </LoadingButton>
+          </Box>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from './CalendarPage.module.css'
+
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const Calendar = () => {
+  const [currentMonthIndex, setCurrentMonthIndex] = useState(0);
+  const totalDays = 30; // общее количество дней месяца
+
+  const prevMonth = () => {
+    setCurrentMonthIndex(prevIndex => (prevIndex === 0 ? 11 : prevIndex - 1));
+  };
+
+  const nextMonth = () => {
+    setCurrentMonthIndex(prevIndex => (prevIndex === 11 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className={styles.calendar}>
+      <div className={styles.month}>
+        <button onClick={prevMonth}>{'<'}</button>
+        <span>{months[currentMonthIndex]}</span>
+        <button onClick={nextMonth}>{'>'}</button>
+      </div>
+      <div className={styles.days}>
+      {Array.from({ length: totalDays }, (_, index) => (
+          <div className="day" key={index+1}>{index+1}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
